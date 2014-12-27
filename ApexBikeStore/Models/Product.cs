7e1f37b08e4928552ec21bf059a621d021a42b4a
phelan1993 <s00126107@mail.itsligo.ie>
2014-12-27 18:19:02 +0000

    fixed relationship between Customer and Transactions tables

---
+++ ApexBikeStore/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace ApexBikeStore.Models
+{
+    public class Product
+    {
+        public ObservableCollection<Product> Items { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return String.Format("{0} ({1})", Name, Price);
+        }
+    }
+}
